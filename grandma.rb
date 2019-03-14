29acde12f79af98009652ca917f9877c015f30fb
Rob O'Gara <rgo594@gmail.com>
2019-03-14 14:48:14 +0000

    Automatically backed up by Learn

---
+++ grandma.rb
@@ -0,0 +1,8 @@
+def speak_to_grandma(a)
+  if a == a.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif a == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
+end
